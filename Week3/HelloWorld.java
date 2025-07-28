72ea8020c9f69d39bd4008a7b58efccf3bf30000
Vikrant Patel <research.vikrantpatel@gmail.com>
2025-07-28 16:23:33 +1000

    I added hahahahaha

--- Week3/HelloWorld.java
+++ Week3/HelloWorld.java
@@ -3,5 +3,6 @@ package Week3;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Welcome to Week 3!!");
+        System.out.println("Hahahahah");
     }
 }
