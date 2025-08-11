569a857d403811b18aa263daa42246f8358936ea
Vikrant Patel <research.vikrantpatel@gmail.com>
2025-08-11 13:35:08 +1000

    After week 5 lecture

---
+++ Week5/Week5_Lecture.java
@@ -0,0 +1,29 @@
+package Week5;
+
+public class Week5_Lecture {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Week 5 lecture: ");
+
+        //Let's print 10 numbers using while loop
+        // int i = 1;
+        // while (i<=10) {
+        //     System.out.println(i);
+        //     i++;
+        // }
+
+        // int i = 1;
+        // // for ( ; i <= 10; ) {
+        // //     System.out.println(i);
+        // //     i++;
+        // // }
+
+        int i = 1;
+        do {
+            System.out.println(i);
+            
+        } while (i<=10);
+
+        System.out.println("This is the end of the program!!");
+
+    }
+}
