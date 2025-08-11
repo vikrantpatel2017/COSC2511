2d36efa89382adeb666e70ec276f94393657b577
Vikrant Patel <research.vikrantpatel@gmail.com>
2025-08-11 17:26:54 +1000

    After Task 3

--- Week5/Week5_Practical_Task1_3.java
+++ Week5/Week5_Practical_Task1_3.java
@@ -1,15 +1,29 @@
 package Week5;
 
+import java.util.Scanner;
+
 public class Week5_Practical_Task1_3 {
     
     public static void main(String[] args) {
         System.out.println("Welcome to the program which will print numbers from 100 to 1 using do while loop!");
 
-        int i = 100;
-        do {
-            System.out.println(i);
-            i--;
-        } while (i>=1);
+        Scanner myScanner = new Scanner(System.in);
+
+        int runningTotal = 0;
+        int temp;
+
+        for (int i = 1; i <= 4 ; i++) {
+            System.out.println("Hey user, enter the " + i + "th number: ");
+            temp = myScanner.nextInt();
+            runningTotal = runningTotal + temp;
+            System.out.println("Running total = "+runningTotal);
+        }
+
+        System.out.println("Final Toal = "+runningTotal);
+
+
+
+
 
 
     }
